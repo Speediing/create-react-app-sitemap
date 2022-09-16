37a8135681a7873e1cf6b80ec1c060efad454a95
Jason Wiker <Jason.wiker@vercel.com>
2022-09-16 13:06:41 -0600

    .

--- src/Routes.js
+++
@@ -1,7 +0,0 @@
-import  React from 'react';
-import { Route, Switch } from "react-router";
-export default ( 
-<Switch>
-    <Route path="/en/about-us" />
-    <Route path="/en/contact-us" />
-</Switch>);
---
+++ src/components.js
@@ -0,0 +1,102 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+export default function BasicExample() {
+  return (
+    <Router>
+    <Switch>
+      <Route path='/' component={Home}/>
+      <Route path='/about' component={About}/>
+      <Route path='/dashboard'  component={Dashboard}/>
+      <Route /> // No-match case
+    </Switch>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+export function Home() {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+         <h2>Home</h2>
+      </div>
+    </div>
+  );
+}
+
+export function About() {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+         <h2>About</h2>
+      </div>
+    </div>
+  );
+}
+
+export function Dashboard() {
+  return (
+    <div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+         <h2>Dashboard</h2>
+      </div>
+    </div>
+  );
+}
+
